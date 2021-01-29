4568f4222b41a43cee2d2bb1d5e805daea375e8f
Andreas Berg <andreas.berg2@student.kyh.se>
2021-01-29 08:14:17 +0100

    initial commit

---
+++ OneTimePad.py
@@ -0,0 +1,21 @@
+
+
+
+
+def main():
+
+    message = input('Enter a message: ')
+
+    # Klartext: ABC
+    # Klartext, numeriskt: 65, 66, 67
+    # Nyckelkrav
+    # 1.
+    # 1. Den måste vara totalt slumpmässig, inga upprepade mönster
+    # 2. Måste vara minst lika långt som meddeladet
+    # 3. Nyckeln får ABSOLUT bara användas en gång
+    # Nyckel: 34, 17, 97
+
+
+
+if __name__=='__main__':
+    main()
